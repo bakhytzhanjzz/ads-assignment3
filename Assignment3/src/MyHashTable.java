6f2a8ee013e5ae10726581a50e46bf4492196aea
Bakhytzhan <bakhytzhanabdilmazhit@gmail.com>
2024-05-11 23:19:40 +0500

    Add files via upload

---
+++ Assignment3/src/MyHashTable.java
@@ -0,0 +1,117 @@
+public class MyHashTable<K, V> {
+    class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+            HashNode<K, V> next;
+
+        }
+        public HashNode<K, V> getNext() {
+            return next;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+    public MyHashTable() {
+        chainArray = new HashNode[11];
+        size = 0;
+    }
+
+    public MyHashTable(int M) {
+        this.M = M;
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+
+    private int hash(K key) {
+        return Math.abs(key.hashCode() % M);
+    }
+
+    public void put(K key, V value) {
+        int index = hash(key);
+        HashNode<K, V> node = chainArray[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        chainArray[index] = new HashNode(key, value);
+        size++;
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+        HashNode<K, V> node = chainArray[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = hash(key);
+        HashNode<K, V> prev = null;
+        HashNode<K, V> node = chainArray[index];
+        while (node != null){
+            if (node.key.equals(key)) {
+                if (prev == null) {
+                    chainArray[index] = node.next;
+                } else {
+                    prev.next = node.next;
+                }
+                size--;
+                return node.value;
+            }
+            prev = node;
+            node = node.next;
+        }
+        return null;
+    }
+
+    public boolean contains(V value) {
+        for (int i = 0; i < M; i++){
+            HashNode<K, V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
+    }
+
+    public K getKey(V value) {
+        for (int i = 0; i < M; i++){
+            HashNode<K, V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)){
+                    return node.key;
+                }
+                node = node.next;
+            }
+        }
+        return null;
+    }
+
+    public HashNode<K, V>[] getChainArray() {
+        return chainArray;
+    }
+}
---
+++ Assignment3/src/MyHashTableTest.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class MyHashTableTest {
+    public static void main(String[] args) {
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>();
+
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            MyTestingClass key = new MyTestingClass(random.nextInt(10000), "Name" + i);
+            Student value = new Student(random.nextInt(10000), "Student" + i);
+            table.put(key, value);
+        }
+
+        int[] bucketCounts = new int[table.getChainArray().length];
+        for (int i = 0; i < table.getChainArray().length; i++) {
+            MyHashTable<MyTestingClass, Student>.HashNode<MyTestingClass, Student> node = table.getChainArray()[i];
+            while (node != null) {
+                bucketCounts[i]++;
+                node = node.getNext();
+            }
+        }
+        for (int i = 0; i < bucketCounts.length; i++) {
+            System.out.println("Bucket " + i + ": " + bucketCounts[i] + " elements");
+        }
+    }
+}
